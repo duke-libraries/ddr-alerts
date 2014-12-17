5f948585033bc3b39c5c50c00157fabcdc399cb6
Jim Coble <jim.coble@duke.edu>
2014-12-17 13:10:33 -0500

    remove dummy app databases; remove passing locals to view partial

--- spec/views/_alert_message.html.erb_spec.rb
+++ spec/views/_alert_message.html.erb_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 RSpec.describe 'ddr/alerts/message/_alert_message.html.erb', type: :view do
 
   let(:msgs) { [ FactoryGirl.create(:message, message: "AAA"), FactoryGirl.create(:message, message: "BBB") ] }
+  before { allow(view).to receive(:alert_messages).and_return(msgs) }
   it "displays the messages" do
-    render partial: 'ddr/alerts/message/alert_message.html.erb', locals: { alert_messages: msgs }
+    render partial: 'ddr/alerts/message/alert_message.html.erb'
     expect(rendered).to match(/AAA/)
     expect(rendered).to match(/BBB/)
   end
